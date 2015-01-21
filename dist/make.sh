496b9c611acc0be4c6922d447146875af96698a1
Roger Chen <roger@rogerhub.com>
2015-01-21 10:01:27 -0800

    Removed symbolic links, and make the script create them

--- dist/make.sh
+++ dist/make.sh
@@ -3,7 +3,17 @@
 set -e
 set -x
 
-test -f ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-disk1.vmdk
-test -f ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box.ovf
+if [[ ! -f ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-disk1.vmdk ]]; then
+    echo "You need to do 'vagrant box add ubuntu/trusty64' first!" >&2
+    exit 1
+fi
+
+if [[ ! -e box.ovf ]]; then
+    ln -s ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box.ovf box.ovf
+fi
+
+if [[ ! -e box-disk1.vmdk ]]; then
+    ln -s ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-disk1.vmdk box-disk1.vmdk
+fi
 
 tar czvf spring2015.box -L manifests modules include box-disk1.vmdk box.ovf Vagrantfile
