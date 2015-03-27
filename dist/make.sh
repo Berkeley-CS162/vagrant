b24034fce0dc63b87696437139ac724f3c5e57c1
Roger Chen <roger@rogerhub.com>
2015-03-26 22:41:49 -0700

    Improve portability of box distribution script

--- dist/make.sh
+++ dist/make.sh
@@ -8,6 +8,11 @@ if [[ ! -f ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-
     exit 1
 fi
 
+if grep "REPLACEME" Vagrantfile; then
+    echo "Please replace the MAC address in dist/Vagrantfile with the actual image MAC" >&2
+    exit 1
+fi
+
 if [[ ! -e box.ovf ]]; then
     ln -s ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box.ovf box.ovf
 fi
@@ -16,4 +21,4 @@ if [[ ! -e box-disk1.vmdk ]]; then
     ln -s ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-disk1.vmdk box-disk1.vmdk
 fi
 
-tar czvf spring2015.box -L manifests modules include box-disk1.vmdk box.ovf Vagrantfile
+tar czvf spring2015.box -h manifests modules include box-disk1.vmdk box.ovf Vagrantfile
