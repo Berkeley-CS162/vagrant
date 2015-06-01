41664139aae31cbfc159df19a18d5c52274d6b9a
Charles Reiss <woggle@apache.org>
2015-06-01 15:29:26 -0700

    Update for summer2015: do not assume vagrantfile is in 14.04 version subdir

--- dist/make.sh
+++ dist/make.sh
@@ -3,8 +3,19 @@
 set -e
 set -x
 
-if [[ ! -f ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-disk1.vmdk ]]; then
+BASE_BOX_ROOT=~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64
+
+if [[ ! -d $BASE_BOX_ROOT ]]; then
     echo "You need to do 'vagrant box add ubuntu/trusty64' first!" >&2
+    exit 1
+fi
+
+BASE_BOX_VERSION=`ls -d1 $BASE_BOX_ROOT/[0-9]* | tail -1`
+
+echo "Detected base box version $BASE_BOX_VERSION"
+
+if [[ ! -f $BASE_BOX_VERSION/virtualbox/box-disk1.vmdk ]]; then
+    echo "Could not find box-disk1.vmdk from base box"
     exit 1
 fi
 
@@ -14,11 +25,11 @@ if grep "REPLACEME" Vagrantfile; then
 fi
 
 if [[ ! -e box.ovf ]]; then
-    ln -s ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box.ovf box.ovf
+    ln -s $BASE_BOX_VERSION/virtualbox/box.ovf box.ovf
 fi
 
 if [[ ! -e box-disk1.vmdk ]]; then
-    ln -s ~/.vagrant.d/boxes/ubuntu-VAGRANTSLASH-trusty64/14.04/virtualbox/box-disk1.vmdk box-disk1.vmdk
+    ln -s $BASE_BOX_VERSION/virtualbox/box-disk1.vmdk box-disk1.vmdk
 fi
 
-tar czvf spring2015.box -h manifests modules include box-disk1.vmdk box.ovf Vagrantfile
+tar czvf summer2015.box -h manifests modules include box-disk1.vmdk box.ovf Vagrantfile
