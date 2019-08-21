cb27a6b7ba7cd6f8ff674d428a5a61aa9c21a055
Sam Kumar <samkumar99@gmail.com>
2019-08-20 22:39:33 -0700

    Clean up after installing Bochs

--- modules/cs162/templates/bochs/install.sh
+++ modules/cs162/templates/bochs/install.sh
@@ -7,7 +7,12 @@ VERSION=2.6.7
 
 wget -O bochs-${VERSION}.tar.gz "http://downloads.sourceforge.net/project/bochs/bochs/${VERSION}/bochs-${VERSION}.tar.gz"
 tar -xvzf bochs-${VERSION}.tar.gz
-cd bochs-${VERSION}/
+pushd bochs-${VERSION}/
 
 ./configure --enable-gdb-stub --with-x --with-x11 --with-term --with-nogui
 sudo make install
+
+# Clean up
+popd
+rm bochs-${VERSION}.tar.gz
+rm -rf bochs-${VERSION}/
