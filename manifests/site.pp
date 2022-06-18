3e2507905dbd37ebac5bbdc958bd02d556ae8a43
rohanku <rohankumar@berkeley.edu>
2022-06-18 10:09:50 -0700

    update manifest and gitignore

--- manifests/site.pp
+++ manifests/site.pp
@@ -59,6 +59,19 @@ node default {
             "glibc-doc",
             "libx32gcc-4.8-dev",
             "libc6-dev-i386",
+            "libtiff5-dev",
+            "libjpeg8-dev",
+            "libopenjp2-7-dev",
+            "zlib1g-dev",
+            "libfreetype6-dev",
+            "liblcms2-dev",
+            "libwebp-dev",
+            "tcl8.6-dev",
+            "tk8.6-dev",
+            "python3-tk",
+            "libharfbuzz-dev",
+            "libfribidi-dev",
+            "libxcb1-dev",
         ]:
           ensure => installed;
         [
