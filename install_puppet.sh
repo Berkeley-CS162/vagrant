8fa11b555406f3813a725390f174fa72e627e8e9
PotatoParser <wilsonqnguyen@gmail.com>
2022-08-23 00:35:11 -0700

    Fall 2022

--- install_puppet.sh
+++ install_puppet.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Override default cmake with latest
+apt update
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+
 apt update
 # Ensures interaction is disabled
 export DEBIAN_FRONTEND=noninteractive
